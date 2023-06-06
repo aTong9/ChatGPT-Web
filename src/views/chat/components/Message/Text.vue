8ad55a9010e70189e170cd17f2a1207222b31650
aTong9 <aTong9@users.noreply.github.com>
2023-06-06 18:26:04 +0800

    feat: 思维导图

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -107,6 +107,7 @@ onUnmounted(() => {
   <div class="text-black" :class="wrapClass">
     <div ref="textRef" class="leading-relaxed break-words">
       <div v-if="!inversion">
+        <div>1</div>
         <div v-if="!asRawText" class="markdown-body" v-html="text" />
         <div v-else class="whitespace-pre-wrap" v-text="text" />
       </div>
